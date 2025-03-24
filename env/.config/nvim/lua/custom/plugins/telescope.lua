fbb452271757b98bee05ac931f786c959b3898d7
Jacob <jacob@Jacobs-MacBook-Air.local>
2025-03-24 12:53:59 -0500

    telescope hidden files

--- env/.config/nvim/lua/custom/plugins/telescope.lua
+++ env/.config/nvim/lua/custom/plugins/telescope.lua
@@ -53,7 +53,12 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			-- vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sf", function()
+				builtin.find_files({
+					hidden = true,
+				})
+			end, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>sy", builtin.lsp_document_symbols, { desc = "[S]earch s[Y]mbols" })
 			vim.keymap.set("n", "<leader>si", builtin.git_files, { desc = "[S]earch Git [I]gnore" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
