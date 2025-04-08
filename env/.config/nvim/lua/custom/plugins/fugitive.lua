1688a2feddd1f300665124e1266a147552a2a5c9
Jacob <jacob@Jacobs-MacBook-Air.local>
2025-04-08 11:24:13 -0500

    remove unused plugins

--- env/.config/nvim/lua/custom/plugins/fugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	keys = {
-		{ "<leader>fb", "<cmd>Git branch<cr>", desc = "[F]ugitive [B]ranches" },
-		{ "<leader>fs", "<cmd>Git<cr>", desc = "[F]ugitive [S]tatus" },
-	},
-}
