7131202987d9cba72c3d2d566f541f772a92938a
Jacob Van Schenck <jacob@overflow.co>
2025-10-07 15:56:14 -0500

    Remove gitsigns functions

--- env/.config/nvim/lua/custom/plugins/gitsigns.lua
+++ env/.config/nvim/lua/custom/plugins/gitsigns.lua
@@ -34,26 +34,9 @@ return {
 				end, { expr = true })
 
 				-- Actions
-				map("n", "<leader>gs", gs.stage_hunk, { desc = "[G]itsign [S]tage hunk" })
-				map("n", "<leader>gr", gs.reset_hunk, { desc = "[G]itsign [R]eset hunk" })
-				map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "[G]itsign [U]nstage hunk" })
-				map("v", "<leader>gs", function()
-					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "[G]itsign [S]tage hunk" })
-				map("v", "<leader>gr", function()
-					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "[G]itsign [R]eset hunk" })
-				map("n", "<leader>gS", gs.stage_buffer, { desc = "[G]itsign [S]tage buffer" })
-				map("n", "<leader>gR", gs.reset_buffer, { desc = "[G]itsign [R]eset buffer" })
-				map("n", "<leader>gp", gs.preview_hunk, { desc = "[G]itsign [P]review hunk" })
 				map("n", "<leader>gb", function()
 					gs.blame_line({ full = true })
 				end, { desc = "[G]itsign [B]lame line" })
-				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "[T]oggle [B]lame line" })
-				map("n", "<leader>gd", gs.diffthis, { desc = "[G]itsign [D]iff this" })
-				map("n", "<leader>gD", function()
-					gs.diffthis("~")
-				end, { desc = "[G]itsign [D]iff this" })
 			end,
 		})
 	end,
