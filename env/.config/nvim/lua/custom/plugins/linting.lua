baa0c8dfb7e76e196eb34342e9b012e4614045a7
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2025-11-03 07:27:11 -0600

    local_features config added

--- env/.config/nvim/lua/custom/plugins/linting.lua
+++ env/.config/nvim/lua/custom/plugins/linting.lua
@@ -1,26 +1,33 @@
+local ok, features = pcall(require, "local_features")
+local default_features = {
+	enable_nvim_lint = true,
+}
+local config = ok and features or default_features
+
 return {
-	-- "mfussenegger/nvim-lint",
-	-- event = {
-	-- 	"BufReadPre",
-	-- 	"BufNewFile",
-	-- },
-	-- config = function()
-	-- 	local lint = require("lint")
-	--
-	-- 	lint.linters_by_ft = {
-	-- 		javascript = { "eslint_d" },
-	-- 		typescript = { "eslint_d" },
-	-- 		javascriptreact = { "eslint_d" },
-	-- 		typescriptreact = { "eslint_d" },
-	-- 	}
-	--
-	-- 	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-	--
-	-- 	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-	-- 		group = lint_augroup,
-	-- 		callback = function()
-	-- 			lint.try_lint()
-	-- 		end,
-	-- 	})
-	-- end,
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	enabled = config.enable_nvim_lint,
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
