e27605d5c8f5afa5a5a14f8f3d480bfecfa020aa
Jacob Van Schenck <jacob@overflow.co>
2026-03-29 14:43:48 -0500

    update quicker package

--- env/.config/nvim/lua/jacob/plugins/quicker.lua
+++ env/.config/nvim/lua/jacob/plugins/quicker.lua
@@ -1,8 +1,24 @@
 return {
 	"stevearc/quicker.nvim",
 	ft = "qf",
-	opts = {},
 	config = function()
-		require("quicker").setup()
+		require("quicker").setup({
+			keys = {
+				{
+					">",
+					function()
+						require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+					end,
+					desc = "Expand quickfix context",
+				},
+				{
+					"<",
+					function()
+						require("quicker").collapse()
+					end,
+					desc = "Collapse quickfix context",
+				},
+			},
+		})
 	end,
 }
